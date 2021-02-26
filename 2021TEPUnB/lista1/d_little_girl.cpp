1511c47b1113951a794a12a8f95c084929dbc705
Pedenite <pedenite@gmail.com>
2021-02-25 21:46:36 -0300

    Tentativas da letra D interessantes (a remover para simplificar)

--- 2021TEPUnB/lista1/d_little_girl.cpp
+++ 2021TEPUnB/lista1/d_little_girl.cpp
@@ -8,15 +8,54 @@ using namespace std;
 #define mp make_pair
 #define ii pair<int,int>
 
+ll get_max(ll n) {
+    if (n == 0) {
+        return 0;
+    }
+
+    ll maior;
+    for (maior = 1; maior < n; maior++) {
+        maior <<= 1;
+    }
+
+    return maior;
+}
+
+ll get_lower_max(ll n) {
+    if (n == 1) {
+        return 0;
+    }
+
+    ll maior;
+    for (maior = 1; maior < n>>1; maior++) {
+        maior <<= 1;
+    }
+
+    return maior;
+}
+
 int main() {
     ll l, r, maior;
 
     cin >> l >> r;
-    for (maior = 1; maior < r; maior++) {
-        maior <<= 1;
+    cout << get_max(l^r) << '\n';
+
+    return 0;
+    
+    if (l == r){
+        maior = 0;
+    } else if (get_max(l) < r) {
+        maior = get_max(r);
+    } else {
+        ll mask = get_lower_max(l);
+        maior = get_lower_max(l^r);
+        // cout << hex << mask << '\n';
+        // maior = ~mask&r;
+        // cout << hex << maior << '\n';
+        // maior ^= mask;
     }
 
-    cout << maior;
+    cout << maior << '\n';
 
     return 0;
 }
