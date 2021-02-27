0fdf9bd7e59d2c63ba311b75ba1bc24befd0f77c
Pedenite <pedenite@gmail.com>
2021-02-27 16:30:02 -0300

    Preparando questao cubos

---
+++ 2021PCUnB/epbb/c_jogo_cubos.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define vi vector<int>
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define ii pair<int,int>
+
+int main() {
+    int n, m, ai, qi;
+    vi a;
+
+    cin >> n >> m;
+    while (n--) {
+        cin >> ai;
+        a.pb(ai);
+    }
+
+    sort(a.begin(), a.end());
+    while (m--) {
+        
+    }
+
+    return 0;
+}
