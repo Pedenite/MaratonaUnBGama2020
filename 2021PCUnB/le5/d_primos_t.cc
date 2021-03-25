eab1c06b2acebef0e14bd2683b3c1c1ffe2b3e08
Pedenite <pedenite@gmail.com>
2021-03-24 23:12:57 -0300

    preparando arquivos

---
+++ 2021PCUnB/le5/d_primos_t.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define vi vector<int>
+#define ll long long
+#define pb push_back
+#define MAX (ll) (1E12+5)
+
+int main() {
+    int q, x;
+
+    scanf("%d", &q);
+    while (q--) {
+        scanf("%d", &x);
+
+    }
+
+    return 0;
+}
