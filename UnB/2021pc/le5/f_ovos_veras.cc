6c4c5bae11777ca570966abc3278554b0554b3ca
Pedenite <pedenite@gmail.com>
2021-03-31 20:37:53 -0300

    Update lista 6 TEP

--- UnB/2021pc/le5/f_ovos_veras.cc
+++ UnB/2021pc/le5/f_ovos_veras.cc
@@ -6,12 +6,33 @@ using namespace std;
 #define pb push_back
 #define mp make_pair
 #define ii pair<int,int>
+#define MAX (int) (1e5+1)
+
+int have_ovo[MAX];
+
+vi divisores(int n){
+    vi divs;
+    int b;
+    
+    for(int i = 1; i*i <= n; i++){
+        if(n % i == 0){
+            b = n/i;
+            divs.push_back(i);
+            if(i != b)
+                divs.push_back(b);
+        }
+    }
+    
+    sort(divs.begin(),divs.end());
+    
+    return divs;
+}
 
 int main() {
     int n, m, x, y;
 
     scanf("%d %d", &n, &m);
-    while (m--) {
+    for (int i = 1; i <= m; i++) {
         scanf("%d %d", &x, &y);
         
     }
--- UnB/2021tep/lista6/d_goldbach_conjecture.cc
+++ UnB/2021tep/lista6/d_goldbach_conjecture.cc
@@ -2,16 +2,50 @@
 using namespace std;
 
 #define vi vector<int>
-#define ll long long
-#define pb push_back
-#define mp make_pair
-#define ii pair<int,int>
+#define MAX (int) (1e6+1)
+
+vi primos;
+vi is_prime(MAX, 1);
+
+void compute_primes() {
+    primos.push_back(2);
+    for (int i = 3; i <= MAX; i += 2) {
+        if (is_prime[i]) {
+            primos.push_back(i);
+            for (int j = i+i; j <= MAX; j += i)
+                is_prime[j] = false;
+        }
+    }
+}
 
 int main() {
-    int n;
+    int n, x, y;
+
+    compute_primes();
 
     while (scanf("%d", &n) && n != 0) {
-        
+        for (int i = 0; primos[i] < n; i++) {
+            x = y = 0;
+            for (int j = 0; primos[j] < n; j++) {
+                if (primos[i] + primos[j] == n) {
+                    x = primos[i];
+                    y = primos[j];
+                    break;
+                } else if (primos[i] + primos[j] > n) {
+                    break;
+                }
+            }
+
+            if (x || y) {
+                break;
+            }
+        }
+
+        if (x || y) {
+            printf("%d = %d + %d\n", n, x, y);
+        } else {
+            printf("Goldbach's conjecture is wrong.\n");
+        }
     }
 
     return 0;
